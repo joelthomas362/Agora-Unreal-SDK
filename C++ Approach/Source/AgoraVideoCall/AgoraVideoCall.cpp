d152bcabc1df23d796f2f8939338933b60754085
joelthomas362 <joelthomas.362@gmail.com>
2020-08-14 11:41:41 -0400

    Made "MultiChannel" StreamMessage Demo

--- C++ Approach/Source/AgoraVideoCall/AgoraVideoCall.cpp
+++
@@ -1,6 +0,0 @@
-//  Copyright (c) 2019 Agora.io. All rights reserved.
-
-#include "AgoraVideoCall.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AgoraVideoCall, "AgoraVideoCall" );
--- C++ Approach/Source/AgoraVideoCall/AgoraVideoCall.h
+++
@@ -1,6 +0,0 @@
-//  Copyright (c) 2019 Agora.io. All rights reserved.
-
-#pragma once
-
-#include "CoreMinimal.h"
-
